adfc7e84331f09fea5767e275e87ec21e0be9c00
ZaryabAlam <zaryabalam97@gmail.com>
2024-05-26 22:14:44 +0500

    from latest ATA somalia

---
+++ android/app/src/main/kotlin/com/devcatstudio/taam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devcatstudio.taam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/home_bottom_nav/controller/agency_balace_controller.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import '../../../app/configs/app_colors.dart';
+import '../../../app/data/data_controller.dart';
+import '../../../app/utils/api_utility/api_url.dart';
+import '../../../app/utils/custom_widgets/gradient_snackbar.dart';
+
+class AgencyBalanceController extends GetxController {
+  final DataController dataController = Get.put(DataController());
+  var isLoading = false.obs;
+  var agencyBalance = 0.0.obs;
+
+  Future<void> loadGetxData() async {
+    await dataController.loadMyData();
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadGetxData();
+  }
+
+  Future<void> fetchAgencyBalance() async {
+    isLoading.value = true;
+
+    try {
+      var headers = {
+        'Content-Type': 'application/json',
+        'authorization': 'Bearer ${dataController.myToken.value}'
+      };
+
+      var response = await http.get(
+          Uri.parse(
+              '${baseURL}api/accounting/balance/${dataController.myRoleSupportID.value}'),
+          headers: headers);
+
+      var jsonData = json.decode(response.body);
+
+      print("**** Agency Balance Controller Response ****");
+      print(" Agency Balance Controller Controller: ${response.body}");
+
+      if (response.statusCode == 200) {
+        if (response.body.isNotEmpty) {
+          agencyBalance.value = double.parse(response.body);
+        }
+        isLoading.value = false;
+      } else {
+        print('Error: ${response.statusCode}');
+        Get.showSnackbar(gradientSnackbar(
+            "Failure",
+            "${jsonData["error"] ?? "Something went wrong"}",
+            AppColors.red,
+            Icons.warning_rounded));
+      }
+    } catch (e) {
+      print('Error: $e');
+      Get.showSnackbar(gradientSnackbar("Failure", "Something went wrong",
+          AppColors.orange, Icons.warning_rounded));
+      isLoading.value = false;
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
